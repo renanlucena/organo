d76a797bad0e411d9f862ae09b51aedcbe4eed8e
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-16 10:06:51 -0300

    implementando o Banner com CSS e React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Banner from './componentes/Banner/Banner';
 function App() {
   return (
     <div className="App">
-      <Banner />
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,7 +1,11 @@
+import './Banner.css';
+
 function Banner(){
    //JSX
     return(
-        <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+        </header>
     )
 }
 
