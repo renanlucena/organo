615aa9e50ae9c0164b604fd6999f83f8b4837083
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-18 11:34:53 -0300

    começando a implementação do colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/renanlucena.png' alt='Renan Lucena'></img>
+        </div>
+        <div className='rodape'>
+            <h4>Renan Lucena</h4>
+            <h5>Front-End</h5>
+        </div>
+    </div>);
+}
+
+export default Colaborador;
