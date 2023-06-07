006ae8f84d40580d48d0f82936643614a856bf9f
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-07 13:59:49 -0300

    migrando o componente CampoTexto para ts

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -1,11 +1,19 @@
 import './CampoTexto.css';
+import React from 'react';
 
+interface CampoTextoProps{
+    aoAlterado: (valor: string) => void
+    placeholder: string
+    label: string
+    valor: string
 
-const CampoTexto = (props) =>{
+}
+
+const CampoTexto = (props: CampoTextoProps) =>{
     const campo = props.label;
     const placeholder = props.placeholder;
 
-    const aoDigitar = (e) => {
+    const aoDigitar = (e: React.ChangeEvent<HTMLInputElement>) => {
         props.aoAlterado(e.target.value);
     }
     return(
